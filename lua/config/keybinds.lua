d1ba8ce2478db31227da2531fdb4e707aa927e39
mwinleng <mwindaacelengwe@gmail.com>
2026-09-19 19:44:34 +0200

    modified:   lua/config/keybinds.lua

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>eq", vim.cmd.q)
+vim.keymap.set("n", "<leader>qq", vim.cmd.q)
+vim.keymap.set("n", "<leader>ww", vim.cmd.w)
